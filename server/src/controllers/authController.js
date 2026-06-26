2681df817076aa5870bc8ad7e89271329dfe190f
Prasad Nalla <nallaprasad1212@gmail.com>
2026-06-26 09:48:13 +0530

    feat(auth): implement user authentication with JWT

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,12 +1,12 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-
-// Register User
+const jwt =require("jsonwebtoken");
+//register user
 const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
-    // Check existing user
+    // Check if user already exists
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
@@ -26,6 +26,7 @@ const registerUser = async (req, res) => {
     });
 
     res.status(201).json({
+      success: true,
       message: "User registered successfully",
       user: {
         id: user._id,
@@ -34,13 +35,67 @@ const registerUser = async (req, res) => {
       },
     });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     res.status(500).json({
-      message: "Server Error",
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    // Generate JWT
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
     });
   }
 };
 
 module.exports = {
   registerUser,
+  loginUser
 };
