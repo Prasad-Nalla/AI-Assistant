f1e5780a507222408bd4f0c8cb8e52becfcfee5e
Prasad Nalla <nallaprasad1212@gmail.com>
2026-06-28 21:17:23 +0530

    refactor(client): migrate frontend from CRA to Vite

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import Login from './pages/Login';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+export default App;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div style={{ maxWidth: "400px", margin: "100px auto" }}>
+      <h2>Login</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <button type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
